b63c3135fb84369b7bf80a66cbd8efca1bea3036
santhoshbehara-cg <55077822+santhoshbehara-cg@users.noreply.github.com>
2019-09-16 11:11:30 +0000

    script

---
+++ migration.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+name=`hostname
+git clone https://github.com/jyotirepo/scb-migration.git`
+cd scb-migration
+python oneclick.py -u chandrakala0113@gmail.com -p Wrkstn11*cgx# -n $name -j SCB-DEMO
