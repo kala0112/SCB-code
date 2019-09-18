fb6d2646110bc00603cd150a2604e42de1662be7
santhoshbehara-cg <55077822+santhoshbehara-cg@users.noreply.github.com>
2019-09-18 10:53:14 +0000

    add

--- migration.sh
+++ migration.sh
@@ -2,4 +2,4 @@
 name=`hostname`
 git clone https://github.com/jyotirepo/scb-migration.git`
 cd scb-migration
-python oneclick.py -u chandrakala0113@gmail.com -p Wrkstn11*cgx# -n $name -j Migration-18th-2019
+python oneclick.py -u chandrakala0113@gmail.com -p Wrkstn11*cgx# -n $name -j scb-mgr
