45c4e10226faf72e713ae98ea233fe33d1de6b28
santhoshbehara-cg <55077822+santhoshbehara-cg@users.noreply.github.com>
2019-09-18 10:09:09 +0000

    add

--- agentinstallation.sh
+++ agentinstallation.sh
@@ -3,6 +3,4 @@ sudo yum install wget -y
 echo "downloading cloudendure agent"
 wget -O ./installer_linux.py https://console.cloudendure.com/installer_linux.py
 echo "installing cloudendure agent"
-file="/var/lib/jenkins/workspace/token.txt"
-name=$(cat "$file")
-sudo python ./installer_linux.py -t $name
+sudo python ./installer_linux.py -t 1B86-6B72-9C92-7D78-31E7-B0CE-803D-4E36-5128-E8C9-4DA3-4A9E-0D58-8BD3-4BB7-DA3E
