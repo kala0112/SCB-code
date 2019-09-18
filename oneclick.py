185a2ac40241584eb218f331c159f4cd10095a38
santhoshbehara-cg <55077822+santhoshbehara-cg@users.noreply.github.com>
2019-09-18 11:44:54 +0000

    add

--- oneclick.py
+++ oneclick.py
@@ -41,7 +41,7 @@ requests.packages.urllib3.disable_warnings()
 
 HOST = 'https://console.cloudendure.com'
 
-lines = [line.rstrip() for line in open('/var/lib/rundeck/test/scb-migration/blue.txt')]
+lines = [line.rstrip() for line in open('/home/ec2-user/scb-migration/blue.txt')]
 INSTANCE_TYPE = lines[0]
 print (INSTANCE_TYPE)
 SUBNET = 'subnet-xxxxxx'
