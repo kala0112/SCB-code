e59b8cf646b501cb6732878a4743aed6b5ba4a8b
santhoshbehara-cg <55077822+santhoshbehara-cg@users.noreply.github.com>
2019-09-18 11:17:52 +0000

    add

--- migration.sh
+++ migration.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 name=`hostname`
-sudo yum install git -y
-git init
 git clone https://github.com/jyotirepo/scb-migration.git
 cd scb-migration
 python oneclick.py -u chandrakala0113@gmail.com -p Wrkstn11*cgx# -n $name -j scb-mgr
